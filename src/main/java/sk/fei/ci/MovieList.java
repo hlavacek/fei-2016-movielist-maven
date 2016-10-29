ed450ba4acc0d3e04eb43e7afb49d4d36af1d852
Hlavacek, Vladimir <vladimir.hlavacek@accenture.com>
2016-10-29 11:58:21 +0200

    Added logback, defined port in procfile.

--- src/main/java/sk/fei/ci/MovieList.java
+++ src/main/java/sk/fei/ci/MovieList.java
@@ -2,9 +2,21 @@ package sk.fei.ci;
 
 import static spark.Spark.*;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class MovieList {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(MovieList.class);
 
 	public static void main(String[] args) {
+		if (args.length > 0) {
+			int port = Integer.parseInt(args[0]);
+			LOG.info("Running application on port {}.", port);
+			port(Integer.parseInt(args[0]));
+		}
 		get("/hello", (req, res) -> "Hello World<br/>Save private ryan");
+		
+		LOG.info("App started");
 	}
 }
