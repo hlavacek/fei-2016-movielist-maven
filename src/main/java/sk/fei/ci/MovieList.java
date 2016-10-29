f5ba5b99cb1663ee55ba5664e580245cbf3f12b1
Hlavacek, Vladimir <vladimir.hlavacek@accenture.com>
2016-10-29 13:07:47 +0200

    Used environment to get the port for the application.

--- src/main/java/sk/fei/ci/MovieList.java
+++ src/main/java/sk/fei/ci/MovieList.java
@@ -6,18 +6,23 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MovieList {
-	
+
 	private static final Logger LOG = LoggerFactory.getLogger(MovieList.class);
 
 	public static void main(String[] args) {
-		if (args.length > 0) {
-			int port = Integer.parseInt(args[0]);
-			LOG.info("Running application on port {}.", port);
-			port(Integer.parseInt(args[0]));
-		}
+		port(getHerokuAssignedPort());
 		get("/hello", (req, res) -> "Hello World<br/>Save private ryan");
 		get("/", (req, res) -> "Index");
-		
+
 		LOG.info("App started");
 	}
+
+	static int getHerokuAssignedPort() {
+		ProcessBuilder processBuilder = new ProcessBuilder();
+		if (processBuilder.environment().get("PORT") != null) {
+			return Integer.parseInt(processBuilder.environment().get("PORT"));
+		}
+		return 4567; 
+	}
+
 }
